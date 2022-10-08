a4e09c49e87f78ad0f47fab9dfde32fd6ccec8f2
Varshini <varshuk01@gmail.com>
2022-10-08 17:26:02 +0530

    contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AddtocartComponent } from './addtocart/addtocart.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { AddtocartComponent } from './addtocart/addtocart.component';
     NavbarComponent,
     HomeComponent,
     AddtocartComponent,
+    ContactComponent,
    
   ],
   imports: [
